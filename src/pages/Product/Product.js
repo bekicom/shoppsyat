cf491603d4fe0f81adb3a9d22348d82171851a4d
maxavorzakon <mmh377631@gmail.com>
2023-05-22 17:22:48 +0500

    hello

---
+++ src/pages/Product/Product.js
@@ -0,0 +1,12 @@
+import React from "react";
+import img1 from"./assets/image.png"
+export function products(){
+    return(
+        <div className="maxa">
+            <div className="left">
+            <img src={img1} alt="" />
+            </div>
+            <div className="right"></div>
+        </div>
+    )
+}
