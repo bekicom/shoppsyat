487eaa230b78d9b7d9eb01215a5db6836afed00f
bekicom <adelkauz6@gmail.com>
2023-05-22 17:02:04 +0500

    ilhomjonga

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Main } from './components/Main/Main';
 
 function App() {
   return (
     <div className="App">
-      <p>salksjajkhjdbhubfbu bhjb</p>
+  <Main/>
       
     </div>
   );
---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Card } from '../../pages/Card'
+
+export  function Main() {
+  return (
+    <div>
+
+        <nav>
+            salom navbar
+        </nav>
+        <Card/>
+      
+    </div>
+  )
+}
