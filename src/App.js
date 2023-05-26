9f1e8bccaf19e56ec5252148ae6e94a6c1dd3a2f
maxavorzakon <mmh377631@gmail.com>
2023-05-26 17:42:27 +0500

    maxa products

--- src/App.js
+++ src/App.js
@@ -5,17 +5,12 @@ function App() {
   return (
 
     <div className="App">
-<<<<<<< HEAD
   <Main/>
     </div>
   );
-=======
       
-      </div>
 
-  )
 
->>>>>>> 072f6367fb97400bbb5531a4874b2970cd0660d4
 }
 
 export default App
