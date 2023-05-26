c10fb6b9de4ecc1f954d1e914de60e0c332ce1ae
chipitosiki <ilkhomjonkodirov2012@bk.ru>
2023-05-26 14:46:14 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import {Thanks} from "../src/pages/Thanks/Thanks.jsx"
-// import { Navbar } from './components/Navbar/Navbar.js';
+
 
 function App() {
   return (
     <div className="App">
-      
-      <Thanks/>
-      
+            
     </div>
   );
 }
